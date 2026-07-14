88b4ca0d2c5e374df3e7e413b1e1c3d5d021687d
Hiiankit <ankit.thrives@gmail.com>
2026-07-14 10:51:21 +0530

    logs changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ message: "hello class sfwsegfwdwe " });
+  res.json({ message: "hello class wifi is not working " });
 });
 
 app.listen(port, () => {
