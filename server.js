9840b219371fb38e1c20d6c13c5e4ae75fa039fc
Hiiankit <ankit.thrives@gmail.com>
2026-07-14 14:08:36 +0530

    webhoook checking

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ message: "ufghgfjhello class wifi is not working " });
+  res.json({ message: "Express server is running " });
 });
 
 app.listen(port, () => {
