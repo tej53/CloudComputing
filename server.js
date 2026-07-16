0b09fe27b574d04ff976fb7c6f6125e0e9b1f15d
tej53 <tej535617@gmail.com>
2026-07-16 11:14:14 +0530

    Devops

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ message: "ci-cd pipeline is working fine " });
+  res.json({ message: "ci-cd pipeline is working fine betey" });
 });
 
 app.listen(port, () => {
